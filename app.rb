00644ff36f8e80c49ac95dd6e6f10d3b0b2c2428
HelenRaven <helenraven@mail.ru>
2021-02-04 16:33:07 +0700

    Correct logic

--- app.rb
+++ app.rb
@@ -1,30 +1,41 @@
 class App
 
+  CORRECT_PATH = "/time"
+  PARAMS_KEY = "format"
+
   def call(env)
-    @response = TimeFormatter.new(env)
-    [status, headers, body]
+    request = Rack::Request.new(env)
+    response(env["REQUEST_PATH"],request.params)
   end
 
   private
 
-  def status
-    if @response.success?
-      @response.invalid_ary.empty? ? 200 : 400
+  def response(path, params)
+    @response = Rack::Response.new
+    headers
+
+    if path == CORRECT_PATH && params[PARAMS_KEY]
+      time = TimeFormatter.new(params[PARAMS_KEY].split(','))
+      time.call
+      status(time.success?)
+      body(time.get_result)
     else
-      404
+      @response.status = 404
     end
+
+    @response.finish
+  end
+
+  def status(success)
+    @response.status = success ? 200 : 400
   end
 
   def headers
-    {"Content-type" => "text/plain"}
+    @response.set_header("Content-type","text/plain")
   end
 
-  def body
-    if @response.invalid_ary.empty?
-      [@response.time_ary*"-"]
-    else
-      ["Unknown time format" + @response.invalid_ary.to_s ]
-    end
+  def body(body)
+    @response.write(body)
   end
 
 end
--- middleware/time_formatter.rb
+++ middleware/time_formatter.rb
@@ -1,49 +1,41 @@
 class TimeFormatter
 
   PARAMS = {
-    year: Time.now.year,
-    month: Time.now.month,
-    day: Time.now.day,
-    hour: Time.now.hour,
-    minute: Time.now.min,
-    second: Time.now.sec
+    year: "%Y",
+    month: "%m",
+    day: "%d",
+    hour: "%H",
+    minute: "%M",
+    second: "%S"
   }
 
-  attr_reader :time_ary, :invalid_ary
-
-  def initialize(env)
-    @env = env
+  def initialize(params)
+    @params = params
     @time_ary = []
     @invalid_ary = []
-    call
   end
 
   def call
-    request = @env["QUERY_STRING"].gsub!("format=",'').split("%2C")
-    if success?
-      time(request)
-      invalid(request)
+    time = Time.now
+
+    @params.each do |param|
+      if PARAMS[param.to_sym]
+        @time_ary << time.strftime(PARAMS[param.to_sym])
+      else
+        @invalid_ary << param
+      end
     end
   end
 
   def success?
-    @env["REQUEST_PATH"] == "/time"
+    @invalid_ary.empty?
   end
 
-  private
-
-  def time(request)
-    request.each do |param|
-      param = param.to_sym
-      @time_ary << PARAMS[param].to_s if PARAMS[param]
-    end
-
-  end
-
-  def invalid(request)
-    request.each do |param|
-      param = param.to_sym
-      @invalid_ary << param.to_s unless PARAMS[param]
+  def get_result
+    if success?
+      @time_ary*"-"
+    else
+      "Unknown time format " + @invalid_ary.to_s
     end
   end
 
